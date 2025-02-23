a64449d3b9d7881ad60d215945e9a3574bd393d4
hachiXhachi <hhachiman26@gmail.com>
2025-02-23 14:44:34 +0800

    service injection

--- Crud12/Program.cs
+++ Crud12/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IEmployee, EmployeeRepository>();
+builder.Services.AddScoped<IUnitofWork, UnitofWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
